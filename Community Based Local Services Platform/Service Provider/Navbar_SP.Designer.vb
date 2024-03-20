1bfc1d0644112c6b07ec5d9c40021cc6c2a56027
addy <addy140603@gmail.com>
2024-03-20 13:16:50 +0530

    Added SP side navbar

---
+++ Community Based Local Services Platform/Service Provider/Navbar_SP.Designer.vb
@@ -0,0 +1,72 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Navbar_SP
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Panel1 = New Panel()
+        BtnLogout = New Button()
+        Panel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Panel1
+        ' 
+        Panel1.Anchor = AnchorStyles.None
+        Panel1.BackColor = Color.FromArgb(CByte(15), CByte(42), CByte(55))
+        Panel1.Controls.Add(BtnLogout)
+        Panel1.Location = New Point(0, 0)
+        Panel1.Margin = New Padding(0)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(1371, 87)
+        Panel1.TabIndex = 0
+        ' 
+        ' BtnLogout
+        ' 
+        BtnLogout.BackColor = Color.FromArgb(CByte(249), CByte(117), CByte(75))
+        BtnLogout.FlatAppearance.BorderSize = 0
+        BtnLogout.FlatStyle = FlatStyle.Flat
+        BtnLogout.Font = New Font("Bahnschrift Light", 12.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        BtnLogout.ForeColor = Color.White
+        BtnLogout.Location = New Point(1178, 24)
+        BtnLogout.Margin = New Padding(0)
+        BtnLogout.Name = "BtnLogout"
+        BtnLogout.Size = New Size(122, 39)
+        BtnLogout.TabIndex = 5
+        BtnLogout.Text = "Logout"
+        BtnLogout.UseVisualStyleBackColor = False
+        ' 
+        ' Navbar_SP
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackgroundImageLayout = ImageLayout.None
+        ClientSize = New Size(1353, 881)
+        Controls.Add(Panel1)
+        Name = "Navbar_SP"
+        Text = "Form1"
+        Panel1.ResumeLayout(False)
+        ResumeLayout(False)
+    End Sub
+
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents BtnLogout As Button
+
+End Class
---
+++ Community Based Local Services Platform/Service Provider/Navbar_SP.vb
@@ -0,0 +1,166 @@
+﻿Public Class Navbar_SP
+
+
+    ' Variable to store the currently active form
+    'Private activeForm As Form = Nothing
+
+    ' Method to set the active form
+    'Private Sub SetActiveForm(ByVal form As Form)
+    '    activeForm = form
+    'End Sub
+    ' 0F2A37
+    ' Method to highlight the active button
+    Public Panel3 As New Panel()
+    Private Sub Navbar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ' Highlight the Home button by default
+        Me.CenterToParent()
+        Me.WindowState = FormWindowState.Normal
+        Panel1.BackColor = ColorTranslator.FromHtml("#0F2A37")
+
+        Dim HomeButton As New Button()
+        HomeButton.Size() = New Size(52, 30)
+
+        HomeButton.BackColor = ColorTranslator.FromHtml("#0F2A37")
+        HomeButton.Location = New Point(630, 18)
+        HomeButton.Text = "Home"
+        HomeButton.ForeColor = Color.White
+        HomeButton.Font = New Font("Bahnschrift Light", 11, FontStyle.Regular)
+        HomeButton.FlatStyle = FlatStyle.Flat
+        HomeButton.FlatAppearance.BorderSize = 0
+        HomeButton.Padding = New Padding(0, 0, 0, 0)
+        HomeButton.TextAlign = ContentAlignment.MiddleCenter
+        HomeButton.AutoSize = True
+
+        Panel1.Controls.Add(HomeButton)
+        Dim AppointmentButton As New Button()
+        AppointmentButton.Size() = New Size(52, 30)
+        AppointmentButton.BackColor = ColorTranslator.FromHtml("#0F2A37")
+        AppointmentButton.Location = New Point(707, 18)
+        AppointmentButton.Text = "Appointments"
+        AppointmentButton.ForeColor = Color.White
+        AppointmentButton.Font = New Font("Bahnschrift Light", 11, FontStyle.Regular)
+        AppointmentButton.FlatStyle = FlatStyle.Flat
+        AppointmentButton.FlatAppearance.BorderSize = 0
+        AppointmentButton.Padding = New Padding(0, 0, 0, 0)
+        AppointmentButton.TextAlign = ContentAlignment.MiddleCenter
+        AppointmentButton.AutoSize = True
+
+        Panel1.Controls.Add(AppointmentButton)
+
+        Dim QueriesButton As New Button()
+        QueriesButton.Size() = New Size(52, 30)
+        QueriesButton.BackColor = ColorTranslator.FromHtml("#0F2A37")
+        QueriesButton.Location = New Point(909, 18)
+        QueriesButton.Text = "Queries"
+        QueriesButton.ForeColor = Color.White
+        QueriesButton.Font = New Font("Bahnschrift Light", 11, FontStyle.Regular)
+        QueriesButton.FlatStyle = FlatStyle.Flat
+        QueriesButton.FlatAppearance.BorderSize = 0
+        QueriesButton.Padding = New Padding(0, 0, 0, 0)
+        QueriesButton.TextAlign = ContentAlignment.MiddleCenter
+        QueriesButton.AutoSize = True
+
+        Panel1.Controls.Add(QueriesButton)
+        AddHandler HomeButton.Click, AddressOf BtnHome_Click
+        AddHandler AppointmentButton.Click, AddressOf BtnAppointment_Click
+        AddHandler QueriesButton.Click, AddressOf BtnQueries_Click
+        Me.Controls.Add(Panel3)
+        Panel3.Size = New Size(1200, 700)
+        Panel3.Location = New Point(0, 0)
+        Panel3.BackColor = Color.Aqua
+        Panel3.Padding = New Padding(0, 0, 0, 0)
+
+        RemovePreviousForm()
+
+        'SetActiveForm(Homepage)
+        Homepage_Customer.Margin = New Padding(0, 0, 0, 0)
+        With Homepage_Customer
+            .TopLevel = False
+            .Dock = DockStyle.Fill
+            Panel3.Controls.Add(Homepage_Customer)
+            .BringToFront()
+            .Show()
+        End With
+    End Sub
+    Private Sub RemovePreviousForm()
+        ' Check if any form is already in Panel5
+        If Panel3.Controls.Count > 0 Then
+            ' Remove the first control (form) from Panel5
+            Panel3.Controls.Clear()
+        End If
+    End Sub
+
+
+
+    Private Sub BtnHome_Click(sender As Object, e As EventArgs)
+        RemovePreviousForm()
+        'SetActiveForm(Homepage)
+        With Homepage_Customer
+            .TopLevel = False
+            .Dock = DockStyle.Fill
+            Panel3.Controls.Add(Homepage_Customer)
+            .BringToFront()
+            .Show()
+        End With
+
+    End Sub
+
+    Private Sub BtnAppointment_Click(sender As Object, e As EventArgs)
+        RemovePreviousForm()
+        'SetActiveForm(Appointment)
+        With Appointment_Customer
+            .TopLevel = False
+            .Dock = DockStyle.Fill
+            Panel3.Controls.Add(Appointment_Customer)
+            .BringToFront()
+            .Show()
+        End With
+
+    End Sub
+
+    Private Sub BtnProfile_Click(sender As Object, e As EventArgs)
+        RemovePreviousForm()
+        'SetActiveForm(Profile)
+        With Profile_Customer
+            .TopLevel = False
+            .Dock = DockStyle.Fill
+            Panel3.Controls.Add(Profile_Customer)
+            .BringToFront()
+            .Show()
+        End With
+    End Sub
+
+    Private Sub BtnQueries_Click(sender As Object, e As EventArgs)
+        RemovePreviousForm()
+        'SetActiveForm(Queries)
+        With Queries_Customer
+            .TopLevel = False
+            .Dock = DockStyle.Fill
+            Panel3.Controls.Add(Queries_Customer)
+            .BringToFront()
+            .Show()
+        End With
+    End Sub
+
+    Private Sub BtnLogout_Click(sender As Object, e As EventArgs) Handles BtnLogout.Click
+        ' here you have add the logic of reverting the state of shared variable for cureent user to null
+        ' Whoever is doing the bankend part will do this part.
+
+        Me.Hide()
+
+        Dim form As New LandingPage()
+        form.Show()
+
+    End Sub
+
+
+
+    Private Sub Form1_FormClosing(ByVal sender As Object, ByVal e As System.Windows.Forms.FormClosingEventArgs) Handles Me.FormClosing
+        ' Ensure that the application exits when the main form is closed
+        Application.Exit()
+    End Sub
+
+    Private Sub Panel1_Paint(sender As Object, e As PaintEventArgs) Handles Panel1.Paint
+
+    End Sub
+End Class
