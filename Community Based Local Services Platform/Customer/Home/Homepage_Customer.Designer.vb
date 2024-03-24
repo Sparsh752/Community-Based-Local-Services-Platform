5817ad388437482d7afc5a3d122f43f7726fea56
Sai-004 <94926227+Sai-004@users.noreply.github.com>
2024-03-24 17:27:53 +0530

    added search panel in customer interface

--- Community Based Local Services Platform/Customer/Home/Homepage_Customer.Designer.vb
+++ Community Based Local Services Platform/Customer/Home/Homepage_Customer.Designer.vb
@@ -23,22 +23,220 @@ Partial Class Homepage_Customer
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Panel1 = New Panel()
+        searchBox = New TextBox()
         Panel2 = New Panel()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        priceLabel = New Label()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        ComboBox1 = New ComboBox()
+        Label4 = New Label()
+        TrackBar1 = New TrackBar()
+        Search = New Button()
+        Panel1.SuspendLayout()
+        CType(TrackBar1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
         ' Panel1
         ' 
-        Panel1.Location = New Point(-10, -3)
+        Panel1.Controls.Add(Search)
+        Panel1.Controls.Add(TrackBar1)
+        Panel1.Controls.Add(Label4)
+        Panel1.Controls.Add(ComboBox1)
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(Label2)
+        Panel1.Controls.Add(Label1)
+        Panel1.Controls.Add(TextBox2)
+        Panel1.Controls.Add(TextBox1)
+        Panel1.Controls.Add(priceLabel)
+        Panel1.Controls.Add(Button4)
+        Panel1.Controls.Add(Button5)
+        Panel1.Controls.Add(Button6)
+        Panel1.Controls.Add(Button3)
+        Panel1.Controls.Add(Button2)
+        Panel1.Controls.Add(Button1)
+        Panel1.Controls.Add(searchBox)
+        Panel1.Location = New Point(-4, -3)
         Panel1.Name = "Panel1"
-        Panel1.Size = New Size(379, 931)
+        Panel1.Size = New Size(373, 931)
         Panel1.TabIndex = 0
+        ' 
+        ' searchBox
+        ' 
+        searchBox.BorderStyle = BorderStyle.FixedSingle
+        searchBox.Font = New Font("Segoe UI", 11.5F)
+        searchBox.Location = New Point(38, 51)
+        searchBox.Name = "searchBox"
+        searchBox.Size = New Size(293, 33)
+        searchBox.TabIndex = 0
         ' 
         ' Panel2
         ' 
         Panel2.Location = New Point(375, -3)
         Panel2.Name = "Panel2"
-        Panel2.Size = New Size(1001, 931)
+        Panel2.Size = New Size(993, 931)
         Panel2.TabIndex = 1
+        ' 
+        ' Button1
+        ' 
+        Button1.Location = New Point(38, 125)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(86, 29)
+        Button1.TabIndex = 0
+        Button1.Text = "Button1"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Location = New Point(130, 125)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(86, 29)
+        Button2.TabIndex = 1
+        Button2.Text = "Button2"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Location = New Point(222, 125)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(86, 29)
+        Button3.TabIndex = 2
+        Button3.Text = "Button3"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.Location = New Point(38, 160)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(86, 29)
+        Button4.TabIndex = 5
+        Button4.Text = "Button4"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.Location = New Point(130, 160)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(86, 29)
+        Button5.TabIndex = 4
+        Button5.Text = "Button5"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.Location = New Point(222, 160)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(86, 29)
+        Button6.TabIndex = 3
+        Button6.Text = "Button6"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' priceLabel
+        ' 
+        priceLabel.AutoSize = True
+        priceLabel.Font = New Font("Segoe UI", 16F)
+        priceLabel.Location = New Point(38, 229)
+        priceLabel.Name = "priceLabel"
+        priceLabel.Size = New Size(74, 37)
+        priceLabel.TabIndex = 0
+        priceLabel.Text = "Price"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BorderStyle = BorderStyle.FixedSingle
+        TextBox1.Font = New Font("Segoe UI", 10.8F)
+        TextBox1.Location = New Point(50, 292)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(106, 31)
+        TextBox1.TabIndex = 6
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BorderStyle = BorderStyle.FixedSingle
+        TextBox2.Font = New Font("Segoe UI", 10.8F)
+        TextBox2.Location = New Point(193, 292)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(106, 31)
+        TextBox2.TabIndex = 7
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI", 10F)
+        Label1.Location = New Point(50, 266)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(39, 23)
+        Label1.TabIndex = 8
+        Label1.Text = "Min"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI", 10F)
+        Label2.Location = New Point(193, 266)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(42, 23)
+        Label2.TabIndex = 9
+        Label2.Text = "Max"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Segoe UI", 16F)
+        Label3.Location = New Point(38, 373)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(119, 37)
+        Label3.TabIndex = 10
+        Label3.Text = "Location"
+        ' 
+        ' ComboBox1
+        ' 
+        ComboBox1.Font = New Font("Segoe UI", 10.8F)
+        ComboBox1.FormattingEnabled = True
+        ComboBox1.Location = New Point(50, 413)
+        ComboBox1.Name = "ComboBox1"
+        ComboBox1.Size = New Size(249, 33)
+        ComboBox1.TabIndex = 0
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Segoe UI", 16F)
+        Label4.Location = New Point(38, 494)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(94, 37)
+        Label4.TabIndex = 11
+        Label4.Text = "Rating"
+        ' 
+        ' TrackBar1
+        ' 
+        TrackBar1.Location = New Point(50, 556)
+        TrackBar1.Name = "TrackBar1"
+        TrackBar1.Size = New Size(249, 56)
+        TrackBar1.TabIndex = 0
+        TrackBar1.TickFrequency = 2
+        TrackBar1.TickStyle = TickStyle.TopLeft
+        ' 
+        ' Search
+        ' 
+        Search.BackColor = Color.Tomato
+        Search.FlatStyle = FlatStyle.Popup
+        Search.Font = New Font("Segoe UI", 12F, FontStyle.Bold)
+        Search.ForeColor = Color.White
+        Search.Location = New Point(110, 662)
+        Search.Name = "Search"
+        Search.Size = New Size(116, 53)
+        Search.TabIndex = 12
+        Search.Text = "Search"
+        Search.UseVisualStyleBackColor = False
         ' 
         ' Homepage_Customer
         ' 
@@ -52,9 +250,29 @@ Partial Class Homepage_Customer
         FormBorderStyle = FormBorderStyle.FixedDialog
         Name = "Homepage_Customer"
         StartPosition = FormStartPosition.CenterParent
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
+        CType(TrackBar1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
 
     Friend WithEvents Panel1 As Panel
     Friend WithEvents Panel2 As Panel
+    Friend WithEvents searchBox As TextBox
+    Friend WithEvents priceLabel As Label
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TrackBar1 As TrackBar
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Search As Button
 End Class
