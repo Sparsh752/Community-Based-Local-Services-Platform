89ef66077ac94f542b4c54a2c71d063c89f5bcee
Himanshi <ghimanshi941@gmail.com>
2024-03-28 17:03:35 +0530

    Added after accepting request and after service completed page for service providers

---
+++ Community Based Local Services Platform/Service Provider/Appointments/TransactionAccepted_SP.Designer.vb
@@ -0,0 +1,186 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class TransactionAccepted_SP
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Label10 = New Label()
+        Label11 = New Label()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Bahnschrift Light", 10F)
+        Label1.ForeColor = Color.Gray
+        Label1.Location = New Point(64, 203)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(66, 21)
+        Label1.TabIndex = 2
+        Label1.Text = "Service"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Bahnschrift Light", 10F)
+        Label2.ForeColor = Color.Gray
+        Label2.Location = New Point(410, 203)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(49, 21)
+        Label2.TabIndex = 3
+        Label2.Text = "Price"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Bahnschrift Light", 10F)
+        Label3.ForeColor = Color.Gray
+        Label3.Location = New Point(65, 283)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(101, 21)
+        Label3.TabIndex = 4
+        Label3.Text = "Booked Slot"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Font = New Font("Bahnschrift Light", 10F)
+        Label4.ForeColor = Color.Gray
+        Label4.Location = New Point(410, 283)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(74, 21)
+        Label4.TabIndex = 5
+        Label4.Text = "Location"
+        ' 
+        ' Label5
+        ' 
+        Label5.AutoSize = True
+        Label5.Font = New Font("Bahnschrift", 20F)
+        Label5.Location = New Point(58, 48)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(120, 41)
+        Label5.TabIndex = 11
+        Label5.Text = "Label5"
+        ' 
+        ' Label6
+        ' 
+        Label6.AutoSize = True
+        Label6.Font = New Font("Bahnschrift Light", 12F)
+        Label6.Location = New Point(64, 98)
+        Label6.MaximumSize = New Size(400, 100)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(70, 24)
+        Label6.TabIndex = 12
+        Label6.Text = "Label6"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Bahnschrift Light", 12F)
+        Label7.Location = New Point(64, 143)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(70, 24)
+        Label7.TabIndex = 13
+        Label7.Text = "Label7"
+        ' 
+        ' Label8
+        ' 
+        Label8.AutoSize = True
+        Label8.Font = New Font("Bahnschrift", 20F)
+        Label8.Location = New Point(58, 224)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(120, 41)
+        Label8.TabIndex = 14
+        Label8.Text = "Label8"
+        ' 
+        ' Label9
+        ' 
+        Label9.AutoSize = True
+        Label9.Font = New Font("Bahnschrift", 20F)
+        Label9.Location = New Point(405, 224)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(118, 41)
+        Label9.TabIndex = 15
+        Label9.Text = "Label9"
+        ' 
+        ' Label10
+        ' 
+        Label10.AutoSize = True
+        Label10.Font = New Font("Bahnschrift", 20F)
+        Label10.Location = New Point(58, 306)
+        Label10.Name = "Label10"
+        Label10.Size = New Size(131, 41)
+        Label10.TabIndex = 16
+        Label10.Text = "Label10"
+        ' 
+        ' Label11
+        ' 
+        Label11.AutoSize = True
+        Label11.Font = New Font("Bahnschrift", 20F)
+        Label11.Location = New Point(405, 306)
+        Label11.Name = "Label11"
+        Label11.Size = New Size(123, 41)
+        Label11.TabIndex = 17
+        Label11.Text = "Label11"
+        ' 
+        ' TransactionAccepted_SP
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(682, 566)
+        Controls.Add(Label11)
+        Controls.Add(Label10)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "TransactionAccepted_SP"
+        Text = "TransactionAccepted_SP"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Label10 As Label
+    Friend WithEvents Label11 As Label
+End Class
