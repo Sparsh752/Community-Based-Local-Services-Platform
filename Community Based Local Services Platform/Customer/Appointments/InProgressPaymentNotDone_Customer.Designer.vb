435f6feef1c480fe97aa7b63bc9d8c09ba4d2e81
Tanisha <tanishasalvi12@gmail.com>
2024-03-28 21:15:12 +0530

    added in progress customer

---
+++ Community Based Local Services Platform/Customer/Appointments/InProgressPaymentNotDone_Customer.Designer.vb
@@ -0,0 +1,332 @@
+﻿
+<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class InProgressPaymentNotDone_Customer
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        Label4 = New Label()
+        Label5 = New Label()
+        Label6 = New Label()
+        TextBox3 = New TextBox()
+        TextBox4 = New TextBox()
+        TextBox5 = New TextBox()
+        TextBox6 = New TextBox()
+        TextBox7 = New TextBox()
+        TextBox8 = New TextBox()
+        TextBox9 = New TextBox()
+        Label7 = New Label()
+        Label8 = New Label()
+        Label9 = New Label()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        SuspendLayout()
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.BorderStyle = BorderStyle.None
+        TextBox1.Font = New Font("Bahnschrift", 20.0F)
+        TextBox1.Location = New Point(84, 44)
+        TextBox1.Margin = New Padding(0)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(245, 49)
+        TextBox1.TabIndex = 0
+        TextBox1.Text = "SP Name"
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.BorderStyle = BorderStyle.None
+        TextBox2.Font = New Font("Bahnschrift", 10.0F)
+        TextBox2.Location = New Point(84, 95)
+        TextBox2.Margin = New Padding(2)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(228, 25)
+        TextBox2.TabIndex = 1
+        TextBox2.Text = "993956584"
+        ' 
+        ' Label1
+        ' 
+        Label1.Font = New Font("Bahnschrift", 7.875F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label1.ForeColor = Color.FromArgb(CByte(136), CByte(136), CByte(136))
+        Label1.Location = New Point(76, 130)
+        Label1.Margin = New Padding(0)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(115, 22)
+        Label1.TabIndex = 2
+        Label1.Text = " Service"
+        ' 
+        ' Label2
+        ' 
+        Label2.Font = New Font("Bahnschrift", 7.875F)
+        Label2.ForeColor = Color.FromArgb(CByte(136), CByte(136), CByte(136))
+        Label2.Location = New Point(76, 191)
+        Label2.Margin = New Padding(2, 0, 2, 0)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(115, 22)
+        Label2.TabIndex = 3
+        Label2.Text = " Location"
+        ' 
+        ' Label3
+        ' 
+        Label3.Font = New Font("Bahnschrift", 7.875F)
+        Label3.ForeColor = Color.FromArgb(CByte(136), CByte(136), CByte(136))
+        Label3.Location = New Point(76, 259)
+        Label3.Margin = New Padding(2, 0, 2, 0)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(115, 22)
+        Label3.TabIndex = 4
+        Label3.Text = " Booked Slot"
+        ' 
+        ' Label4
+        ' 
+        Label4.Font = New Font("Bahnschrift", 7.875F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        Label4.ForeColor = Color.FromArgb(CByte(136), CByte(136), CByte(136))
+        Label4.Location = New Point(527, 136)
+        Label4.Margin = New Padding(2, 0, 2, 0)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(115, 22)
+        Label4.TabIndex = 5
+        Label4.Text = "Price"
+        ' 
+        ' Label5
+        ' 
+        Label5.Font = New Font("Bahnschrift", 7.875F)
+        Label5.ForeColor = Color.FromArgb(CByte(136), CByte(136), CByte(136))
+        Label5.Location = New Point(527, 191)
+        Label5.Margin = New Padding(2, 0, 2, 0)
+        Label5.Name = "Label5"
+        Label5.Size = New Size(115, 22)
+        Label5.TabIndex = 6
+        Label5.Text = "Advance Paid"
+        ' 
+        ' Label6
+        ' 
+        Label6.Font = New Font("Bahnschrift", 7.875F)
+        Label6.ForeColor = Color.FromArgb(CByte(136), CByte(136), CByte(136))
+        Label6.Location = New Point(527, 259)
+        Label6.Margin = New Padding(2, 0, 2, 0)
+        Label6.Name = "Label6"
+        Label6.Size = New Size(154, 22)
+        Label6.TabIndex = 7
+        Label6.Text = "Remaining Balance"
+        ' 
+        ' TextBox3
+        ' 
+        TextBox3.BorderStyle = BorderStyle.None
+        TextBox3.Font = New Font("Bahnschrift Light", 16.0F)
+        TextBox3.Location = New Point(84, 152)
+        TextBox3.Margin = New Padding(0)
+        TextBox3.Name = "TextBox3"
+        TextBox3.Size = New Size(245, 39)
+        TextBox3.TabIndex = 8
+        TextBox3.Text = "Interior Design"
+        ' 
+        ' TextBox4
+        ' 
+        TextBox4.BorderStyle = BorderStyle.None
+        TextBox4.Font = New Font("Bahnschrift Light", 16.0F)
+        TextBox4.Location = New Point(84, 213)
+        TextBox4.Margin = New Padding(0)
+        TextBox4.Name = "TextBox4"
+        TextBox4.Size = New Size(245, 39)
+        TextBox4.TabIndex = 9
+        TextBox4.Text = "Guwahati"
+        ' 
+        ' TextBox5
+        ' 
+        TextBox5.BorderStyle = BorderStyle.None
+        TextBox5.Font = New Font("Bahnschrift Light", 16.0F)
+        TextBox5.Location = New Point(84, 281)
+        TextBox5.Margin = New Padding(0)
+        TextBox5.Name = "TextBox5"
+        TextBox5.Size = New Size(335, 39)
+        TextBox5.TabIndex = 10
+        TextBox5.Text = "23-Mar-2024 09:00 AM"
+        ' 
+        ' TextBox6
+        ' 
+        TextBox6.BorderStyle = BorderStyle.None
+        TextBox6.Font = New Font("Bahnschrift Light", 16.0F)
+        TextBox6.Location = New Point(531, 153)
+        TextBox6.Margin = New Padding(0)
+        TextBox6.Name = "TextBox6"
+        TextBox6.Size = New Size(245, 39)
+        TextBox6.TabIndex = 11
+        TextBox6.Text = "Rs. 15000"
+        ' 
+        ' TextBox7
+        ' 
+        TextBox7.BorderStyle = BorderStyle.None
+        TextBox7.Font = New Font("Bahnschrift Light", 16.0F)
+        TextBox7.Location = New Point(531, 213)
+        TextBox7.Margin = New Padding(0)
+        TextBox7.Name = "TextBox7"
+        TextBox7.Size = New Size(245, 39)
+        TextBox7.TabIndex = 12
+        TextBox7.Text = "Rs. 7500"
+        ' 
+        ' TextBox8
+        ' 
+        TextBox8.BorderStyle = BorderStyle.None
+        TextBox8.Font = New Font("Bahnschrift Light", 16.0F)
+        TextBox8.Location = New Point(531, 281)
+        TextBox8.Margin = New Padding(0)
+        TextBox8.Name = "TextBox8"
+        TextBox8.Size = New Size(245, 39)
+        TextBox8.TabIndex = 13
+        TextBox8.Text = "Rs. 7500"
+        ' 
+        ' TextBox9
+        ' 
+        TextBox9.BorderStyle = BorderStyle.None
+        TextBox9.Font = New Font("Bahnschrift Light", 16.0F)
+        TextBox9.Location = New Point(166, 470)
+        TextBox9.Margin = New Padding(0)
+        TextBox9.Name = "TextBox9"
+        TextBox9.Size = New Size(122, 39)
+        TextBox9.TabIndex = 14
+        TextBox9.Text = "781039"
+        ' 
+        ' Label7
+        ' 
+        Label7.AutoSize = True
+        Label7.Font = New Font("Bahnschrift", 16.0F)
+        Label7.Location = New Point(76, 470)
+        Label7.Margin = New Padding(0)
+        Label7.Name = "Label7"
+        Label7.Size = New Size(90, 39)
+        Label7.TabIndex = 15
+        Label7.Text = "OTP :"
+        ' 
+        ' Label8
+        ' 
+        Label8.Font = New Font("Bahnschrift", 10.0F)
+        Label8.Location = New Point(76, 527)
+        Label8.Margin = New Padding(0)
+        Label8.Name = "Label8"
+        Label8.Size = New Size(526, 27)
+        Label8.TabIndex = 16
+        Label8.Text = "Share this otp with the service provider"
+        ' 
+        ' Label9
+        ' 
+        Label9.Font = New Font("Bahnschrift", 10.0F)
+        Label9.Location = New Point(76, 554)
+        Label9.Margin = New Padding(0)
+        Label9.Name = "Label9"
+        Label9.Size = New Size(308, 27)
+        Label9.TabIndex = 17
+        Label9.Text = "after completion of the service."
+        ' 
+        ' Button1
+        ' 
+        Button1.Cursor = Cursors.Hand
+        Button1.Location = New Point(84, 350)
+        Button1.Margin = New Padding(0)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(89, 23)
+        Button1.TabIndex = 18
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.Cursor = Cursors.Hand
+        Button2.Location = New Point(84, 417)
+        Button2.Margin = New Padding(0)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(182, 23)
+        Button2.TabIndex = 19
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.Cursor = Cursors.Hand
+        Button3.Location = New Point(527, 417)
+        Button3.Margin = New Padding(0)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(182, 23)
+        Button3.TabIndex = 20
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' InProgressPaymentNotDone_Customer
+        ' 
+        AutoScaleDimensions = New SizeF(10.0F, 25.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        ClientSize = New Size(879, 609)
+        Controls.Add(Button3)
+        Controls.Add(Button2)
+        Controls.Add(Button1)
+        Controls.Add(Label9)
+        Controls.Add(Label8)
+        Controls.Add(Label7)
+        Controls.Add(TextBox9)
+        Controls.Add(TextBox8)
+        Controls.Add(TextBox7)
+        Controls.Add(TextBox6)
+        Controls.Add(TextBox5)
+        Controls.Add(TextBox4)
+        Controls.Add(TextBox3)
+        Controls.Add(Label6)
+        Controls.Add(Label5)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Cursor = Cursors.Default
+        FormBorderStyle = FormBorderStyle.None
+        Margin = New Padding(2)
+        Name = "InProgressPaymentNotDone_Customer"
+        Text = "Appointmentdetail"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Label6 As Label
+    Friend WithEvents TextBox3 As TextBox
+    Friend WithEvents TextBox4 As TextBox
+    Friend WithEvents TextBox5 As TextBox
+    Friend WithEvents TextBox6 As TextBox
+    Friend WithEvents TextBox7 As TextBox
+    Friend WithEvents TextBox8 As TextBox
+    Friend WithEvents TextBox9 As TextBox
+    Friend WithEvents Label7 As Label
+    Friend WithEvents Label8 As Label
+    Friend WithEvents Label9 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
