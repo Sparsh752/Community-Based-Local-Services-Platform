714e040e82664e0454b2fb058df384cb5601d229
Ajay-durgesam <ajaydurgesam@gmail.com>
2024-03-29 23:56:16 +0530

    Revert "Customer_profile inner screen Frontend done"
    This reverts commit e2330514a877e0a3dd942ff1aae8059b39ccc6b7.

--- Community Based Local Services Platform/Resource1.Designer.vb
+++ Community Based Local Services Platform/Resource1.Designer.vb
@@ -77,26 +77,6 @@ Namespace My.Resources
         '''<summary>
         '''  Looks up a localized resource of type System.Drawing.Bitmap.
         '''</summary>
-        Friend Shared ReadOnly Property Customer_Profile() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("Customer_Profile", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
-        Friend Shared ReadOnly Property displayPicture() As System.Drawing.Bitmap
-            Get
-                Dim obj As Object = ResourceManager.GetObject("displayPicture", resourceCulture)
-                Return CType(obj,System.Drawing.Bitmap)
-            End Get
-        End Property
-        
-        '''<summary>
-        '''  Looks up a localized resource of type System.Drawing.Bitmap.
-        '''</summary>
         Friend Shared ReadOnly Property proceedtopay() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("proceedtopay", resourceCulture)
