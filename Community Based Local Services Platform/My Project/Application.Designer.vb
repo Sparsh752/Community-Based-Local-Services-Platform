f00f0ad868525dacf4d127bb116229468a2e7851
mynknight <mayankchoudhary651@gmail.com>
2024-03-30 19:39:36 +0530

    Register Page validation checks

--- Community Based Local Services Platform/My Project/Application.Designer.vb
+++ Community Based Local Services Platform/My Project/Application.Designer.vb
@@ -33,7 +33,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Community_Based_Local_Services_Platform.TwoFA
+            Me.MainForm = Global.Community_Based_Local_Services_Platform.Register1
         End Sub
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
