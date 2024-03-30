d6c75ac1c6fcb7a2ae684dc244796ca4f7dbd485
Ajay-durgesam <ajaydurgesam@gmail.com>
2024-03-30 13:32:32 +0530

    corrected small error in frontend

--- Community Based Local Services Platform/Customer/Profile/Profile_Customer.Designer.vb
+++ Community Based Local Services Platform/Customer/Profile/Profile_Customer.Designer.vb
@@ -34,15 +34,17 @@ Partial Class Profile_Customer
         Label2 = New Label()
         Label3 = New Label()
         Label4 = New Label()
-        Label5 = New Label()
+        Panel1 = New Panel()
+        Button1 = New Button()
         CType(customerProfilePicture, ComponentModel.ISupportInitialize).BeginInit()
+        Panel1.SuspendLayout()
         SuspendLayout()
         ' 
         ' customerProfilePicture
         ' 
         customerProfilePicture.BackgroundImage = My.Resources.Resource1.displayPicture
         customerProfilePicture.BackgroundImageLayout = ImageLayout.Stretch
-        customerProfilePicture.Location = New Point(215, 66)
+        customerProfilePicture.Location = New Point(215, 131)
         customerProfilePicture.Name = "customerProfilePicture"
         customerProfilePicture.Size = New Size(370, 443)
         customerProfilePicture.TabIndex = 0
@@ -53,7 +55,7 @@ Partial Class Profile_Customer
         customerName_tb.BackColor = Color.WhiteSmoke
         customerName_tb.BorderStyle = BorderStyle.None
         customerName_tb.Font = New Font("Bahnschrift SemiCondensed", 32.25F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
-        customerName_tb.Location = New Point(629, 66)
+        customerName_tb.Location = New Point(629, 131)
         customerName_tb.Name = "customerName_tb"
         customerName_tb.Size = New Size(327, 52)
         customerName_tb.TabIndex = 1
@@ -63,7 +65,7 @@ Partial Class Profile_Customer
         ' 
         email_tb.BorderStyle = BorderStyle.FixedSingle
         email_tb.Font = New Font("Bahnschrift Light", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        email_tb.Location = New Point(629, 171)
+        email_tb.Location = New Point(482, 150)
         email_tb.Name = "email_tb"
         email_tb.Size = New Size(332, 33)
         email_tb.TabIndex = 2
@@ -73,7 +75,7 @@ Partial Class Profile_Customer
         ' 
         contact_tb.BorderStyle = BorderStyle.FixedSingle
         contact_tb.Font = New Font("Bahnschrift Light", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        contact_tb.Location = New Point(629, 237)
+        contact_tb.Location = New Point(482, 214)
         contact_tb.Name = "contact_tb"
         contact_tb.Size = New Size(332, 33)
         contact_tb.TabIndex = 3
@@ -83,7 +85,7 @@ Partial Class Profile_Customer
         ' 
         address_tb.BorderStyle = BorderStyle.FixedSingle
         address_tb.Font = New Font("Bahnschrift Light", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        address_tb.Location = New Point(629, 303)
+        address_tb.Location = New Point(482, 280)
         address_tb.Multiline = True
         address_tb.Name = "address_tb"
         address_tb.Size = New Size(332, 109)
@@ -94,7 +96,7 @@ Partial Class Profile_Customer
         ' 
         location_tb.BorderStyle = BorderStyle.FixedSingle
         location_tb.Font = New Font("Bahnschrift Light", 15.75F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        location_tb.Location = New Point(629, 447)
+        location_tb.Location = New Point(482, 424)
         location_tb.Name = "location_tb"
         location_tb.Size = New Size(332, 33)
         location_tb.TabIndex = 5
@@ -106,7 +108,7 @@ Partial Class Profile_Customer
         Label1.BackColor = Color.WhiteSmoke
         Label1.Font = New Font("Bahnschrift", 12.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label1.ForeColor = Color.Black
-        Label1.Location = New Point(629, 149)
+        Label1.Location = New Point(629, 220)
         Label1.Name = "Label1"
         Label1.Size = New Size(51, 19)
         Label1.TabIndex = 6
@@ -118,7 +120,7 @@ Partial Class Profile_Customer
         Label2.BackColor = Color.WhiteSmoke
         Label2.Font = New Font("Bahnschrift", 12.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label2.ForeColor = Color.Black
-        Label2.Location = New Point(629, 215)
+        Label2.Location = New Point(629, 287)
         Label2.Name = "Label2"
         Label2.Size = New Size(64, 19)
         Label2.TabIndex = 7
@@ -130,7 +132,7 @@ Partial Class Profile_Customer
         Label3.BackColor = Color.WhiteSmoke
         Label3.Font = New Font("Bahnschrift", 12.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label3.ForeColor = Color.Black
-        Label3.Location = New Point(629, 281)
+        Label3.Location = New Point(482, 258)
         Label3.Name = "Label3"
         Label3.Size = New Size(69, 19)
         Label3.TabIndex = 8
@@ -142,45 +144,53 @@ Partial Class Profile_Customer
         Label4.BackColor = Color.WhiteSmoke
         Label4.Font = New Font("Bahnschrift", 12.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
         Label4.ForeColor = Color.Black
-        Label4.Location = New Point(629, 425)
+        Label4.Location = New Point(482, 402)
         Label4.Name = "Label4"
         Label4.Size = New Size(71, 19)
         Label4.TabIndex = 9
         Label4.Text = "Location"
         ' 
-        ' Label5
+        ' Panel1
         ' 
-        Label5.AutoSize = True
-        Label5.BackColor = Color.FromArgb(CByte(249), CByte(117), CByte(75))
-        Label5.Cursor = Cursors.Hand
-        Label5.Font = New Font("Bahnschrift", 12.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
-        Label5.ForeColor = Color.White
-        Label5.Location = New Point(519, 551)
-        Label5.Name = "Label5"
-        Label5.Size = New Size(115, 19)
-        Label5.TabIndex = 10
-        Label5.Text = "Update Details"
+        Panel1.BackColor = Color.WhiteSmoke
+        Panel1.Controls.Add(email_tb)
+        Panel1.Controls.Add(address_tb)
+        Panel1.Controls.Add(Label4)
+        Panel1.Controls.Add(contact_tb)
+        Panel1.Controls.Add(Label3)
+        Panel1.Controls.Add(location_tb)
+        Panel1.Location = New Point(147, 92)
+        Panel1.Name = "Panel1"
+        Panel1.Size = New Size(865, 580)
+        Panel1.TabIndex = 11
+        ' 
+        ' Button1
+        ' 
+        Button1.BackColor = Color.FromArgb(CByte(249), CByte(117), CByte(75))
+        Button1.Cursor = Cursors.Hand
+        Button1.FlatStyle = FlatStyle.Flat
+        Button1.Font = New Font("Bahnschrift SemiBold", 12.0F, FontStyle.Bold, GraphicsUnit.Point, CByte(0))
+        Button1.Location = New Point(510, 614)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(139, 33)
+        Button1.TabIndex = 10
+        Button1.Text = "Update Details"
+        Button1.UseVisualStyleBackColor = False
         ' 
         ' Profile_Customer
         ' 
         AutoScaleDimensions = New SizeF(7.0F, 15.0F)
         AutoScaleMode = AutoScaleMode.Font
-        BackColor = SystemColors.Control
-        BackgroundImage = My.Resources.Resource1.Customer_Profile
+        BackColor = Color.White
         BackgroundImageLayout = ImageLayout.Stretch
-        ClientSize = New Size(1194, 629)
+        ClientSize = New Size(1194, 694)
         ControlBox = False
-        Controls.Add(Label5)
-        Controls.Add(Label4)
-        Controls.Add(Label3)
+        Controls.Add(Button1)
         Controls.Add(Label2)
         Controls.Add(Label1)
-        Controls.Add(location_tb)
-        Controls.Add(address_tb)
-        Controls.Add(contact_tb)
-        Controls.Add(email_tb)
         Controls.Add(customerName_tb)
         Controls.Add(customerProfilePicture)
+        Controls.Add(Panel1)
         DoubleBuffered = True
         ForeColor = SystemColors.Control
         FormBorderStyle = FormBorderStyle.FixedDialog
@@ -189,6 +199,8 @@ Partial Class Profile_Customer
         StartPosition = FormStartPosition.CenterParent
         TopMost = True
         CType(customerProfilePicture, ComponentModel.ISupportInitialize).EndInit()
+        Panel1.ResumeLayout(False)
+        Panel1.PerformLayout()
         ResumeLayout(False)
         PerformLayout()
     End Sub
@@ -203,5 +215,6 @@ Partial Class Profile_Customer
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
     Friend WithEvents Label4 As Label
-    Friend WithEvents Label5 As Label
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents Button1 As Button
 End Class
