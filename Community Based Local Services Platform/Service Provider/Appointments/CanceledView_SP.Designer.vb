670727c26601fe894c7558b175456170ab0b8c2d
Tanisha <tanishasalvi12@gmail.com>
2024-03-30 23:12:40 +0530

    added rejected & canceled views for sp

---
+++ Community Based Local Services Platform/Service Provider/Appointments/CanceledView_SP.Designer.vb
@@ -0,0 +1,225 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class CanceledView_SP
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        RichTextBox11 = New RichTextBox()
+        RichTextBox10 = New RichTextBox()
+        RichTextBox9 = New RichTextBox()
+        RichTextBox8 = New RichTextBox()
+        RichTextBox7 = New RichTextBox()
+        RichTextBox6 = New RichTextBox()
+        RichTextBox5 = New RichTextBox()
+        RichTextBox4 = New RichTextBox()
+        RichTextBox3 = New RichTextBox()
+        RichTextBox2 = New RichTextBox()
+        RichTextBox1 = New RichTextBox()
+        SuspendLayout()
+        ' 
+        ' RichTextBox11
+        ' 
+        RichTextBox11.BackColor = Color.White
+        RichTextBox11.BorderStyle = BorderStyle.None
+        RichTextBox11.Font = New Font("Bahnschrift", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        RichTextBox11.ForeColor = Color.Gray
+        RichTextBox11.Location = New Point(593, 231)
+        RichTextBox11.Margin = New Padding(4)
+        RichTextBox11.Name = "RichTextBox11"
+        RichTextBox11.ReadOnly = True
+        RichTextBox11.ScrollBars = RichTextBoxScrollBars.None
+        RichTextBox11.Size = New Size(156, 26)
+        RichTextBox11.TabIndex = 29
+        RichTextBox11.Text = "Location"
+        ' 
+        ' RichTextBox10
+        ' 
+        RichTextBox10.BackColor = Color.White
+        RichTextBox10.BorderStyle = BorderStyle.None
+        RichTextBox10.Font = New Font("Bahnschrift", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        RichTextBox10.ForeColor = Color.Gray
+        RichTextBox10.Location = New Point(95, 231)
+        RichTextBox10.Margin = New Padding(4)
+        RichTextBox10.Name = "RichTextBox10"
+        RichTextBox10.ReadOnly = True
+        RichTextBox10.ScrollBars = RichTextBoxScrollBars.None
+        RichTextBox10.Size = New Size(156, 26)
+        RichTextBox10.TabIndex = 28
+        RichTextBox10.Text = "Booked slot"
+        ' 
+        ' RichTextBox9
+        ' 
+        RichTextBox9.BackColor = Color.White
+        RichTextBox9.BorderStyle = BorderStyle.None
+        RichTextBox9.Font = New Font("Bahnschrift", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        RichTextBox9.ForeColor = Color.Gray
+        RichTextBox9.Location = New Point(593, 147)
+        RichTextBox9.Margin = New Padding(4)
+        RichTextBox9.Name = "RichTextBox9"
+        RichTextBox9.ReadOnly = True
+        RichTextBox9.ScrollBars = RichTextBoxScrollBars.None
+        RichTextBox9.Size = New Size(156, 26)
+        RichTextBox9.TabIndex = 27
+        RichTextBox9.Text = "Price"
+        ' 
+        ' RichTextBox8
+        ' 
+        RichTextBox8.BackColor = Color.White
+        RichTextBox8.BorderStyle = BorderStyle.None
+        RichTextBox8.Font = New Font("Bahnschrift", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        RichTextBox8.ForeColor = Color.Gray
+        RichTextBox8.Location = New Point(95, 147)
+        RichTextBox8.Margin = New Padding(4)
+        RichTextBox8.Name = "RichTextBox8"
+        RichTextBox8.ReadOnly = True
+        RichTextBox8.ScrollBars = RichTextBoxScrollBars.None
+        RichTextBox8.Size = New Size(156, 26)
+        RichTextBox8.TabIndex = 26
+        RichTextBox8.Text = "Service"
+        ' 
+        ' RichTextBox7
+        ' 
+        RichTextBox7.BackColor = Color.White
+        RichTextBox7.BorderStyle = BorderStyle.None
+        RichTextBox7.Font = New Font("Bahnschrift", 16F)
+        RichTextBox7.Location = New Point(95, 55)
+        RichTextBox7.Margin = New Padding(4)
+        RichTextBox7.Name = "RichTextBox7"
+        RichTextBox7.ReadOnly = True
+        RichTextBox7.ScrollBars = RichTextBoxScrollBars.None
+        RichTextBox7.Size = New Size(429, 52)
+        RichTextBox7.TabIndex = 25
+        RichTextBox7.Text = "Customer Name"
+        ' 
+        ' RichTextBox6
+        ' 
+        RichTextBox6.BackColor = Color.White
+        RichTextBox6.BorderStyle = BorderStyle.None
+        RichTextBox6.Font = New Font("Microsoft Sans Serif", 20F)
+        RichTextBox6.ForeColor = Color.FromArgb(CByte(255), CByte(0), CByte(0))
+        RichTextBox6.Location = New Point(95, 384)
+        RichTextBox6.Margin = New Padding(4)
+        RichTextBox6.Name = "RichTextBox6"
+        RichTextBox6.ReadOnly = True
+        RichTextBox6.ScrollBars = RichTextBoxScrollBars.None
+        RichTextBox6.Size = New Size(734, 75)
+        RichTextBox6.TabIndex = 30
+        RichTextBox6.Text = "This booking was canceled by customer."
+        ' 
+        ' RichTextBox5
+        ' 
+        RichTextBox5.BackColor = Color.White
+        RichTextBox5.BorderStyle = BorderStyle.None
+        RichTextBox5.Font = New Font("Bahnschrift", 16F)
+        RichTextBox5.Location = New Point(593, 265)
+        RichTextBox5.Margin = New Padding(4)
+        RichTextBox5.Name = "RichTextBox5"
+        RichTextBox5.ScrollBars = RichTextBoxScrollBars.None
+        RichTextBox5.Size = New Size(156, 35)
+        RichTextBox5.TabIndex = 24
+        RichTextBox5.Text = "Guwahati"
+        ' 
+        ' RichTextBox4
+        ' 
+        RichTextBox4.BackColor = Color.White
+        RichTextBox4.BorderStyle = BorderStyle.None
+        RichTextBox4.Font = New Font("Bahnschrift", 16F)
+        RichTextBox4.Location = New Point(95, 265)
+        RichTextBox4.Margin = New Padding(4)
+        RichTextBox4.Name = "RichTextBox4"
+        RichTextBox4.ScrollBars = RichTextBoxScrollBars.None
+        RichTextBox4.Size = New Size(398, 35)
+        RichTextBox4.TabIndex = 23
+        RichTextBox4.Text = "23-Mar-2024 09:00 Am"
+        ' 
+        ' RichTextBox3
+        ' 
+        RichTextBox3.BackColor = Color.White
+        RichTextBox3.BorderStyle = BorderStyle.None
+        RichTextBox3.Font = New Font("Bahnschrift", 16F)
+        RichTextBox3.Location = New Point(593, 171)
+        RichTextBox3.Margin = New Padding(4)
+        RichTextBox3.Name = "RichTextBox3"
+        RichTextBox3.ScrollBars = RichTextBoxScrollBars.None
+        RichTextBox3.Size = New Size(156, 35)
+        RichTextBox3.TabIndex = 22
+        RichTextBox3.Text = "Rs.123000"
+        ' 
+        ' RichTextBox2
+        ' 
+        RichTextBox2.BackColor = Color.White
+        RichTextBox2.BorderStyle = BorderStyle.None
+        RichTextBox2.Font = New Font("Bahnschrift", 16F)
+        RichTextBox2.Location = New Point(95, 171)
+        RichTextBox2.Margin = New Padding(4)
+        RichTextBox2.Name = "RichTextBox2"
+        RichTextBox2.ScrollBars = RichTextBoxScrollBars.None
+        RichTextBox2.Size = New Size(371, 52)
+        RichTextBox2.TabIndex = 21
+        RichTextBox2.Text = "Interior Design"
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.BackColor = Color.White
+        RichTextBox1.BorderStyle = BorderStyle.None
+        RichTextBox1.Font = New Font("Segoe UI", 9F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        RichTextBox1.Location = New Point(95, 101)
+        RichTextBox1.Margin = New Padding(4)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.ScrollBars = RichTextBoxScrollBars.None
+        RichTextBox1.Size = New Size(371, 35)
+        RichTextBox1.TabIndex = 20
+        RichTextBox1.Text = "Sitaram"
+        ' 
+        ' CanceledView_SP
+        ' 
+        AutoScaleDimensions = New SizeF(10F, 25F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = Color.White
+        BackgroundImageLayout = ImageLayout.None
+        ClientSize = New Size(1500, 794)
+        Controls.Add(RichTextBox11)
+        Controls.Add(RichTextBox10)
+        Controls.Add(RichTextBox9)
+        Controls.Add(RichTextBox8)
+        Controls.Add(RichTextBox7)
+        Controls.Add(RichTextBox6)
+        Controls.Add(RichTextBox5)
+        Controls.Add(RichTextBox4)
+        Controls.Add(RichTextBox3)
+        Controls.Add(RichTextBox2)
+        Controls.Add(RichTextBox1)
+        Name = "CanceledView_SP"
+        Text = "cancelled_by_SP"
+        ResumeLayout(False)
+    End Sub
+    Friend WithEvents RichTextBox11 As RichTextBox
+    Friend WithEvents RichTextBox10 As RichTextBox
+    Friend WithEvents RichTextBox9 As RichTextBox
+    Friend WithEvents RichTextBox8 As RichTextBox
+    Friend WithEvents RichTextBox7 As RichTextBox
+    Friend WithEvents RichTextBox6 As RichTextBox
+    Friend WithEvents RichTextBox5 As RichTextBox
+    Friend WithEvents RichTextBox4 As RichTextBox
+    Friend WithEvents RichTextBox3 As RichTextBox
+    Friend WithEvents RichTextBox2 As RichTextBox
+    Friend WithEvents RichTextBox1 As RichTextBox
+End Class
---
+++ Community Based Local Services Platform/Service Provider/Appointments/CanceledView_SP.vb
@@ -0,0 +1,6 @@
+﻿Public Class CanceledView_SP
+    Private Sub CancelledView_SP_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Me.Size() = New Size(1200, 635)
+        Me.FormBorderStyle = FormBorderStyle.None
+    End Sub
+End Class
