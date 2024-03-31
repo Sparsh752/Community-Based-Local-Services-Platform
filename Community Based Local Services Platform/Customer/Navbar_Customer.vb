504cb2c61233fe62d02b88b4ff14485cc09a440e
Sparsh Mittal <sparshmittal2311@gmail.com>
2024-03-31 14:07:50 +0530

    Panel3 made global

--- Community Based Local Services Platform/Customer/Navbar_Customer.vb
+++ Community Based Local Services Platform/Customer/Navbar_Customer.vb
@@ -13,6 +13,7 @@
     Public Panel3 As New Panel()
     Private Sub Navbar_Load(sender As Object, e As EventArgs) Handles MyBase.Load
         ' Highlight the Home button by default
+        SessionManager.Panel3 = Panel3
         Me.CenterToParent()
         Me.WindowState = FormWindowState.Normal
         Panel1.BackColor = ColorTranslator.FromHtml("#0F2A37")
