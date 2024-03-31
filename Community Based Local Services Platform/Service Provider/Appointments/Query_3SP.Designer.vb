802b41271c9f3837b46540bec91dfc645e615845
Tanisha <tanishasalvi12@gmail.com>
2024-04-01 00:54:22 +0530

    modfied raise query nav

--- Community Based Local Services Platform/Service Provider/Appointments/Query_3SP.Designer.vb
+++ Community Based Local Services Platform/Service Provider/Appointments/Query_3SP.Designer.vb
@@ -25,37 +25,41 @@ Partial Class Query_3SP
         Panel1 = New Panel()
         ComboBox1 = New ComboBox()
         Button1 = New Button()
-        RichTextBox1 = New RichTextBox()
         Label5 = New Label()
         Label4 = New Label()
         Label3 = New Label()
         Label2 = New Label()
         Label1 = New Label()
+        Panel2 = New Panel()
+        RichTextBox1 = New RichTextBox()
         Panel1.SuspendLayout()
+        Panel2.SuspendLayout()
         SuspendLayout()
         ' 
         ' Panel1
         ' 
         Panel1.Controls.Add(ComboBox1)
         Panel1.Controls.Add(Button1)
-        Panel1.Controls.Add(RichTextBox1)
         Panel1.Controls.Add(Label5)
         Panel1.Controls.Add(Label4)
         Panel1.Controls.Add(Label3)
         Panel1.Controls.Add(Label2)
         Panel1.Controls.Add(Label1)
-        Panel1.Location = New Point(400, 100)
+        Panel1.Controls.Add(Panel2)
+        Panel1.Location = New Point(500, 125)
+        Panel1.Margin = New Padding(4)
         Panel1.Name = "Panel1"
-        Panel1.Size = New Size(458, 507)
+        Panel1.Size = New Size(572, 634)
         Panel1.TabIndex = 0
         ' 
         ' ComboBox1
         ' 
         ComboBox1.Font = New Font("Bahnschrift Light", 12F)
         ComboBox1.FormattingEnabled = True
-        ComboBox1.Location = New Point(40, 201)
+        ComboBox1.Location = New Point(50, 251)
+        ComboBox1.Margin = New Padding(4)
         ComboBox1.Name = "ComboBox1"
-        ComboBox1.Size = New Size(354, 32)
+        ComboBox1.Size = New Size(442, 37)
         ComboBox1.TabIndex = 7
         ' 
         ' Button1
@@ -64,31 +68,23 @@ Partial Class Query_3SP
         Button1.FlatStyle = FlatStyle.Flat
         Button1.Font = New Font("Bahnschrift", 10F)
         Button1.ForeColor = Color.White
-        Button1.Location = New Point(139, 448)
+        Button1.Location = New Point(174, 560)
+        Button1.Margin = New Padding(4)
         Button1.Name = "Button1"
-        Button1.Size = New Size(159, 36)
+        Button1.Size = New Size(199, 45)
         Button1.TabIndex = 6
         Button1.Text = "Send"
         Button1.UseVisualStyleBackColor = False
-        ' 
-        ' RichTextBox1
-        ' 
-        RichTextBox1.BorderStyle = BorderStyle.FixedSingle
-        RichTextBox1.Font = New Font("Bahnschrift Light", 10F)
-        RichTextBox1.Location = New Point(40, 298)
-        RichTextBox1.Name = "RichTextBox1"
-        RichTextBox1.Size = New Size(354, 132)
-        RichTextBox1.TabIndex = 5
-        RichTextBox1.Text = ""
         ' 
         ' Label5
         ' 
         Label5.AutoSize = True
         Label5.Font = New Font("Bahnschrift Light", 10F)
         Label5.ForeColor = SystemColors.ControlDarkDark
-        Label5.Location = New Point(34, 259)
+        Label5.Location = New Point(42, 324)
+        Label5.Margin = New Padding(4, 0, 4, 0)
         Label5.Name = "Label5"
-        Label5.Size = New Size(144, 21)
+        Label5.Size = New Size(172, 24)
         Label5.TabIndex = 4
         Label5.Text = "Query Description"
         ' 
@@ -97,9 +93,10 @@ Partial Class Query_3SP
         Label4.AutoSize = True
         Label4.Font = New Font("Bahnschrift Light", 10F)
         Label4.ForeColor = SystemColors.ControlDarkDark
-        Label4.Location = New Point(34, 177)
+        Label4.Location = New Point(42, 221)
+        Label4.Margin = New Padding(4, 0, 4, 0)
         Label4.Name = "Label4"
-        Label4.Size = New Size(92, 21)
+        Label4.Size = New Size(110, 24)
         Label4.TabIndex = 2
         Label4.Text = "Query Type"
         ' 
@@ -107,9 +104,10 @@ Partial Class Query_3SP
         ' 
         Label3.AutoSize = True
         Label3.Font = New Font("Bahnschrift", 20F)
-        Label3.Location = New Point(29, 115)
+        Label3.Location = New Point(36, 144)
+        Label3.Margin = New Padding(4, 0, 4, 0)
         Label3.Name = "Label3"
-        Label3.Size = New Size(103, 41)
+        Label3.Size = New Size(120, 48)
         Label3.TabIndex = 1
         Label3.Text = "12345"
         ' 
@@ -118,9 +116,10 @@ Partial Class Query_3SP
         Label2.AutoSize = True
         Label2.Font = New Font("Bahnschrift Light", 10F)
         Label2.ForeColor = SystemColors.ControlDarkDark
-        Label2.Location = New Point(34, 94)
+        Label2.Location = New Point(42, 118)
+        Label2.Margin = New Padding(4, 0, 4, 0)
         Label2.Name = "Label2"
-        Label2.Size = New Size(70, 21)
+        Label2.Size = New Size(81, 24)
         Label2.TabIndex = 1
         Label2.Text = "Appt. ID"
         ' 
@@ -128,22 +127,45 @@ Partial Class Query_3SP
         ' 
         Label1.AutoSize = True
         Label1.Font = New Font("Bahnschrift", 20F)
-        Label1.Location = New Point(28, 33)
+        Label1.Location = New Point(35, 41)
+        Label1.Margin = New Padding(4, 0, 4, 0)
         Label1.Name = "Label1"
-        Label1.Size = New Size(202, 41)
+        Label1.Size = New Size(234, 48)
         Label1.TabIndex = 0
         Label1.Text = "Raise Query"
         ' 
+        ' Panel2
+        ' 
+        Panel2.BorderStyle = BorderStyle.FixedSingle
+        Panel2.Controls.Add(RichTextBox1)
+        Panel2.Location = New Point(50, 367)
+        Panel2.Name = "Panel2"
+        Panel2.Size = New Size(442, 169)
+        Panel2.TabIndex = 1
+        ' 
+        ' RichTextBox1
+        ' 
+        RichTextBox1.BorderStyle = BorderStyle.None
+        RichTextBox1.Font = New Font("Bahnschrift Light", 10F)
+        RichTextBox1.Location = New Point(8, 4)
+        RichTextBox1.Margin = New Padding(4)
+        RichTextBox1.Name = "RichTextBox1"
+        RichTextBox1.Size = New Size(424, 161)
+        RichTextBox1.TabIndex = 5
+        RichTextBox1.Text = ""
+        ' 
         ' Query_3SP
         ' 
-        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleDimensions = New SizeF(10F, 25F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(1165, 588)
+        ClientSize = New Size(1456, 735)
         Controls.Add(Panel1)
+        Margin = New Padding(4)
         Name = "Query_3SP"
         Text = "Query_3SP"
         Panel1.ResumeLayout(False)
         Panel1.PerformLayout()
+        Panel2.ResumeLayout(False)
         ResumeLayout(False)
     End Sub
 
@@ -156,4 +178,5 @@ Partial Class Query_3SP
     Friend WithEvents RichTextBox1 As RichTextBox
     Friend WithEvents Button1 As Button
     Friend WithEvents ComboBox1 As ComboBox
+    Friend WithEvents Panel2 As Panel
 End Class
