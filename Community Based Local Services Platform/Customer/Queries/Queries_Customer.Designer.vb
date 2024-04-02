64d44c520cac145616b2cc572b630fdd79d9bf70
Tanisha <tanishasalvi12@gmail.com>
2024-04-02 10:22:22 +0530

    updates customer queries

--- Community Based Local Services Platform/Customer/Queries/Queries_Customer.Designer.vb
+++ Community Based Local Services Platform/Customer/Queries/Queries_Customer.Designer.vb
@@ -22,17 +22,127 @@ Partial Class Queries_Customer
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
+        Dim DataGridViewCellStyle1 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        Dim DataGridViewCellStyle3 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        Dim DataGridViewCellStyle2 As DataGridViewCellStyle = New DataGridViewCellStyle()
+        DataGridView1 = New DataGridView()
+        QueryType = New DataGridViewTextBoxColumn()
+        AppointmentID = New DataGridViewTextBoxColumn()
+        QueryDate = New DataGridViewTextBoxColumn()
+        Description = New DataGridViewTextBoxColumn()
+        Status = New DataGridViewTextBoxColumn()
+        View = New DataGridViewButtonColumn()
+        CType(DataGridView1, ComponentModel.ISupportInitialize).BeginInit()
         SuspendLayout()
         ' 
-        ' Queries
+        ' DataGridView1
         ' 
-        AutoScaleDimensions = New SizeF(7F, 15F)
+        DataGridView1.AllowUserToAddRows = False
+        DataGridView1.AllowUserToDeleteRows = False
+        DataGridView1.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+        DataGridView1.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.DisplayedCells
+        DataGridView1.BackgroundColor = Color.White
+        DataGridViewCellStyle1.Alignment = DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle1.BackColor = Color.Tomato
+        DataGridViewCellStyle1.Font = New Font("Bahnschrift", 9.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        DataGridViewCellStyle1.ForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle1.NullValue = "Hello"
+        DataGridViewCellStyle1.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle1.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle1.WrapMode = DataGridViewTriState.True
+        DataGridView1.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle1
+        DataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        DataGridView1.Columns.AddRange(New DataGridViewColumn() {QueryType, AppointmentID, QueryDate, Description, Status, View})
+        DataGridView1.Cursor = Cursors.Hand
+        DataGridViewCellStyle3.Alignment = DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle3.BackColor = Color.White
+        DataGridViewCellStyle3.Font = New Font("Bahnschrift", 9.0F, FontStyle.Regular, GraphicsUnit.Point, CByte(0))
+        DataGridViewCellStyle3.ForeColor = SystemColors.ActiveCaptionText
+        DataGridViewCellStyle3.SelectionBackColor = SystemColors.Highlight
+        DataGridViewCellStyle3.SelectionForeColor = SystemColors.HighlightText
+        DataGridViewCellStyle3.WrapMode = DataGridViewTriState.False
+        DataGridView1.DefaultCellStyle = DataGridViewCellStyle3
+        DataGridView1.EnableHeadersVisualStyles = False
+        DataGridView1.GridColor = SystemColors.HighlightText
+        DataGridView1.Location = New Point(254, 236)
+        DataGridView1.MultiSelect = False
+        DataGridView1.Name = "DataGridView1"
+        DataGridView1.ReadOnly = True
+        DataGridView1.RowHeadersVisible = False
+        DataGridView1.RowHeadersWidth = 51
+        DataGridView1.ScrollBars = ScrollBars.None
+        DataGridView1.Size = New Size(1103, 174)
+        DataGridView1.TabIndex = 2
+        ' 
+        ' QueryType
+        ' 
+        QueryType.HeaderText = "Query Type"
+        QueryType.MinimumWidth = 6
+        QueryType.Name = "QueryType"
+        QueryType.ReadOnly = True
+        ' 
+        ' AppointmentID
+        ' 
+        AppointmentID.HeaderText = "Appointment ID"
+        AppointmentID.MinimumWidth = 6
+        AppointmentID.Name = "AppointmentID"
+        AppointmentID.ReadOnly = True
+        ' 
+        ' QueryDate
+        ' 
+        QueryDate.HeaderText = "Query Date"
+        QueryDate.MinimumWidth = 6
+        QueryDate.Name = "QueryDate"
+        QueryDate.ReadOnly = True
+        ' 
+        ' Description
+        ' 
+        Description.HeaderText = "Description"
+        Description.MinimumWidth = 6
+        Description.Name = "Description"
+        Description.ReadOnly = True
+        ' 
+        ' Status
+        ' 
+        Status.HeaderText = "Status"
+        Status.MinimumWidth = 6
+        Status.Name = "Status"
+        Status.ReadOnly = True
+        ' 
+        ' View
+        ' 
+        DataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleCenter
+        DataGridViewCellStyle2.BackColor = Color.FromArgb(CByte(15), CByte(42), CByte(55))
+        DataGridViewCellStyle2.ForeColor = Color.White
+        View.DefaultCellStyle = DataGridViewCellStyle2
+        View.FlatStyle = FlatStyle.Popup
+        View.HeaderText = ""
+        View.MinimumWidth = 6
+        View.Name = "View"
+        View.ReadOnly = True
+        View.Resizable = DataGridViewTriState.False
+        View.Text = "View"
+        View.UseColumnTextForButtonValue = True
+        ' 
+        ' Queries_Customer
+        ' 
+        AutoScaleDimensions = New SizeF(10.0F, 25.0F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(1194, 694)
+        ClientSize = New Size(1706, 1100)
         ControlBox = False
+        Controls.Add(DataGridView1)
         FormBorderStyle = FormBorderStyle.FixedDialog
-        Margin = New Padding(3, 2, 3, 2)
-        Name = "Queries"
+        Margin = New Padding(4, 3, 4, 3)
+        Name = "Queries_Customer"
+        CType(DataGridView1, ComponentModel.ISupportInitialize).EndInit()
         ResumeLayout(False)
     End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents QueryType As DataGridViewTextBoxColumn
+    Friend WithEvents AppointmentID As DataGridViewTextBoxColumn
+    Friend WithEvents QueryDate As DataGridViewTextBoxColumn
+    Friend WithEvents Description As DataGridViewTextBoxColumn
+    Friend WithEvents Status As DataGridViewTextBoxColumn
+    Friend WithEvents View As DataGridViewButtonColumn
 End Class
--- Community Based Local Services Platform/Customer/Queries/Queries_Customer.vb
+++ Community Based Local Services Platform/Customer/Queries/Queries_Customer.vb
@@ -1,3 +1,60 @@
 ﻿Public Class Queries_Customer
 
+    Private Sub DataGridView1_CellPainting(sender As Object, e As DataGridViewCellPaintingEventArgs)
+        If e.RowIndex >= 0 AndAlso e.ColumnIndex >= 0 AndAlso e.ColumnIndex <= 5 Then
+            ' Draw cell border
+            e.Paint(e.CellBounds, DataGridViewPaintParts.All)
+
+            ' Draw right border
+            Dim rectRight As New Rectangle(e.CellBounds.Right - 1, e.CellBounds.Top, 1, e.CellBounds.Height)
+            e.Graphics.FillRectangle(Brushes.Black, rectRight)
+
+            ' Draw bottom border
+            Dim rectBottom As New Rectangle(e.CellBounds.Left, e.CellBounds.Bottom - 1, e.CellBounds.Width, 1)
+            e.Graphics.FillRectangle(Brushes.Black, rectBottom)
+
+            e.Handled = True
+        End If
+    End Sub
+
+    Private Sub Queries_Customer_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'DataGridView1.ColumnCount = 6 ' Set the number of columns to 4
+
+        DataGridView1.Columns(0).HeaderText = "Query Type"
+        DataGridView1.Columns(1).HeaderText = "Appointment ID"
+        DataGridView1.Columns(2).HeaderText = "Query Date"
+        DataGridView1.Columns(3).HeaderText = "Description"
+        DataGridView1.Columns(4).HeaderText = "Status"
+        DataGridView1.Columns(5).HeaderText = ""
+
+        Me.Font = New Font(SessionManager.font_family, 9, FontStyle.Regular)
+        Me.FormBorderStyle = FormBorderStyle.None
+        DataGridView1.ColumnHeadersDefaultCellStyle.Font = New Font(SessionManager.font_family, 9, FontStyle.Regular)
+        DataGridView1.DefaultCellStyle.Font = New Font(SessionManager.font_family, 9, FontStyle.Regular)
+        Me.Size = New Size(1200, 700)
+        DataGridView1.Location = New Point(200, 120)
+
+        DataGridView1.Size = New Size(800, 300)
+
+        DataGridView1.Rows.Add("Payment", "123", "xyz", "xyz", "Pending")
+        DataGridView1.Rows.Add("Payment", "123", "xyz", "xyz", "Pending")
+        DataGridView1.Rows.Add("Payment", "123", "xyz", "xyz", "Pending")
+        DataGridView1.Rows.Add("Payment", "123", "xyz", "xyz", "Pending")
+        DataGridView1.Rows.Add("Payment", "123", "xyz", "xyz", "Pending")
+        DataGridView1.Rows.Add("Payment", "123", "xyz", "xyz", "Pending")
+        DataGridView1.Rows.Add("Payment", "123", "xyz", "xyz", "Pending")
+        DataGridView1.Rows.Add("Payment", "123", "xyz", "xyz", "Pending")
+        DataGridView1.Rows.Add("Payment", "123", "xyz", "xyz", "Pending")
+        DataGridView1.Rows.Add("Payment", "123", "xyz", "xyz", "Pending")
+        DataGridView1.Rows.Add("Payment", "123", "xyz", "xyz", "Pending")
+        DataGridView1.Rows.Add("Payment", "123", "xyz", "xyz", "Pending")
+
+        AddHandler DataGridView1.CellContentClick, AddressOf DataGridView1_CellContentClick
+
+    End Sub
+
+    Private Sub DataGridView1_CellContentClick(sender As Object, e As DataGridViewCellEventArgs)
+
+    End Sub
+
 End Class
